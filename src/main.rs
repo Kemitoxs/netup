0a859d552573d07ee9ef6e715cf3b0841a87406c
Alexander <alexander.zivny-hartig@protonmail.com>
2024-04-21 17:47:44 +0200

    add gui & verbosity flag

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
+use std::{process::exit, sync::mpsc, thread};
+
 use clap::{arg, Command};
-use tracing::{info, Level};
+use tracing::{info, trace, Level};
 
 mod client;
+mod gui;
 mod server;
 mod utils;
 
 fn parse_args() -> Command {
     clap::Command::new("netup")
         .subcommand_required(true)
+        .arg(arg!(-v --verbose "Print debug information"))
         .subcommand(
             Command::new("server")
                 .about("Run as the server")
@@ -16,16 +20,26 @@ fn parse_args() -> Command {
         .subcommand(
             Command::new("client")
                 .about("Run as the client")
+                .arg(arg!(-g --gui "Run with GUI"))
                 .arg(arg!(<ADDR> "The address to connect to (Eg. localhost:56701)")),
         )
 }
 
 fn main() {
     let cmd = parse_args().get_matches();
-    tracing_subscriber::fmt()
-        .with_max_level(Level::TRACE)
-        .init();
+    let verbose = cmd.get_flag("verbose");
+    let mut fmt = tracing_subscriber::fmt();
+
+    if verbose {
+        fmt = fmt.with_max_level(Level::TRACE);
+    } else {
+        fmt = fmt.with_max_level(Level::INFO);
+    }
+
+    fmt.init();
+
     info!("Starting application");
+    trace!("Verbose flag: {}", verbose);
 
     match cmd.subcommand() {
         Some(("server", args)) => {
@@ -33,8 +47,19 @@ fn main() {
             server::run_server(addr).unwrap();
         }
         Some(("client", args)) => {
-            let addr = args.get_one::<String>("ADDR").unwrap();
-            client::run_client(addr).unwrap();
+            let addr = args.get_one::<String>("ADDR").unwrap().to_string();
+            let gui = args.get_flag("gui");
+
+            let (tx, rx) = mpsc::channel();
+            let t1 = thread::spawn(move || client::run_client(addr, tx).unwrap());
+
+            if gui {
+                info!("Running with GUI");
+                gui::run_gui(rx);
+                exit(0);
+            }
+
+            t1.join().unwrap();
         }
         _ => {}
     }
